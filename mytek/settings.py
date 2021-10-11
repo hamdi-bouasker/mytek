1f6f66b10b8bcb86f16bd54266ebb894692be018
IT-Support-L2 <itech@cyberservices.com>
2021-10-11 01:06:33 +0100

    Update settings.py

--- mytek/settings.py
+++ mytek/settings.py
@@ -45,18 +45,13 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-     'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
-
-CACHE_MIDDLEWARE_ALIAS = 'default'
-CACHE_MIDDLEWARE_SECONDS = 600
 
 SESSION_EXPIRE_SECONDS = 900  # 15 minutes
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
