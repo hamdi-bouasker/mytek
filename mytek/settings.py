7d2a8365e8cfb030080e8b6a676b986439e84849
IT-Support-L2 <itech@cyberservices.com>
2021-10-23 00:44:01 +0100

    Update settings.py

--- mytek/settings.py
+++ mytek/settings.py
@@ -85,11 +85,24 @@ AUTH_USER_MODEL ='accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == True:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    
+else:
+    DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "NAME": config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('HOST'),
+        "PORT": config('PORT),
     }
 }
 
