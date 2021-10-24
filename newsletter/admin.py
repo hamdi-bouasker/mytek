d15245744a58bb9e3075fa6ae6ec097506dec611
IT-Support-L2 <itech@cyberservices.com>
2021-10-24 23:11:38 +0100

    read_only fileds

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Subscribe, Contact
 
-admin.site.register(Subscribe)
-admin.site.register(Contact)
+
+class SubscribeAdmin(admin.ModelAdmin):
+    readonly_fields = ('email', 'title', 'body', 'created_at')
+    
+class ContactAdmin(admin.ModelAdmin):
+    readonly_fields = ('email', 'created_at')
+  
+admin.site.register(SubscribeAdmin, Subscribe)
+admin.site.register(ContactAdmin, Contact)
